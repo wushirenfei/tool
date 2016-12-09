0628a9119247d9a983924390593068b224189f92
alex <zyzcyxm@163.com>
2016-12-09 17:35:28 +0800

    add

--- host_update/host_update.py
+++ host_update/host_update.py
@@ -1,1 +1,24 @@
 # -*- coding=utf-8 -*-
+
+import re
+import requests
+import pprint
+
+
+def update_hosts(url=None):
+    if not url:
+        url = 'https://github.com/racaljk/hosts/blob/master/hosts'
+
+        response = requests.get(url)
+
+        res = re.findall(r'<td id="LC(.*)</td>', response.text)
+
+        file = open('./hosts','w')
+        for line in res:
+            file.write('{}\n'.format(re.findall(r'>(.*)', line)[0]))
+        file.flush()
+        file.close()
+
+
+if __name__ == '__main__':
+    update_hosts()
